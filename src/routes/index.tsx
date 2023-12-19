0372a6e58c8e8451cd13060be38530919b7a3e60
Érick Fernandes <erickfernandess2@gmail.com>
2023-12-19 15:27:12 -0500

    Configurando o tema com MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={ <Button>Teste</Button> } />
+            <Route path="/pagina-inicial" element={ <Button variant="contained" color="primary">Teste</Button> } />
 
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />  
         </Routes>
